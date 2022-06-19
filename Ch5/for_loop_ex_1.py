4431a71ea845aefe981db9e299c133db169a9bef
sranjanB <srbehura95@gmail.com>
2022-06-19 10:47:26 +0530

    T430 Update

---
+++ Ch5/for_loop_ex_1.py
@@ -0,0 +1,5 @@
+print("Printing 1 to 10 using for loop.")
+for current_value in range (1, 11):
+	print(current_value)
+
+print("This Job is Done, Exiting for loop.")
---
+++ Ch5/while_loop_ex_1.py
@@ -0,0 +1,7 @@
+# printing 1 to 10 using while loop
+print("Printing 1 to 10 using while loop.")
+current_value = 1
+while current_value <= 10 :
+	print(current_value)
+	current_value += 1
+print("Job has been done, Exiting from loop.")

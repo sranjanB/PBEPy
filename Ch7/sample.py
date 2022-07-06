878fe48d984c15135995e050681503e26aa65369
sranjanB <srbehura95@gmail.com>
2022-07-06 09:03:51 +0530

    Added new

--- Ch7/sample.py
+++ Ch7/sample.py
@@ -1,8 +1,16 @@
+import my_library as ml
+
 def print_details(name, age):
     """
-    This function accept parameters name and age.
+    This function accept arameters name and age.
     """
     print(f"Hello, {name}.\nI guess you are {age} year old !")
-    
+
 print(print_details.__doc__)
 print_details("Sonu", 10)
+result = ml.make_total_double(20, 30.5)
+print(result)
+
+print(ml.my_list)
+new_list = list(map(lambda x:x+5, ml.my_list))
+print(new_list)
---
+++ Ch7/using_modules-example_1.py
@@ -0,0 +1,7 @@
+from my_library import my_list, my_dictionary, make_total_double
+print("Accesing some list elemnet:")
+print(f"my_dictionary[1]={my_dictionary[1]}")
+
+print(f"\nUsing double function now.")
+result = make_total_double(20, 30.5)
+print(result)
